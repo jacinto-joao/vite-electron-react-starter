76a3c44f90878cedc97cb5901485cba0ae9cd272
Jacinto Joao <jacintotbrc@gmail.com>
2022-01-19 16:37:38 +0200

    updating camera permission

--- packages/renderer/src/components/user-profile/UserProfile.tsx
+++ packages/renderer/src/components/user-profile/UserProfile.tsx
@@ -14,7 +14,6 @@ const user: User = {
 function UserProfile() {
   return (
     <div className='user-profile'>
-
         <Avatar/>
         <Details user={user} />
     </div>
--- packages/renderer/src/components/user-profile/partials/Avatar.tsx
+++ packages/renderer/src/components/user-profile/partials/Avatar.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Webcam from 'webcamjs';
 
 function Avatar() {
+
+  useEffect(() => {
+    initCamera();
+  },[]);
+
+  const initCamera = async  () => {
+    await  Webcam.set({
+        width: 320,
+        height: 240,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+      });
+    await	Webcam.attach( '#camera-container');
+  };
+
   return (
     <>
       <div className="avatar"></div>
       <button className='btn btn__selfie'>
         Take selfie;
       </button>
+
+      <div id="camera-container"  className='camera-container'></div>
     </>
   );
 }
