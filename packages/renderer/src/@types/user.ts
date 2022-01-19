9742e325c884bde043973b6b6355a1682db76f6c
Jacinto Joao <jacintotbrc@gmail.com>
2022-01-19 12:41:24 +0200

    adding user components and types

---
+++ packages/renderer/src/@types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    mobileNumber: number;
+    nickName: string;
+    avatar: string;
+    cover?: string;
+}
