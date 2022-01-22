fcea00ef088b572bbc0ea4dfef87a1f5d27534e1
Jacinto Joao <jacintotbrc@gmail.com>
2022-01-23 00:26:30 +0200

    adding webcam component

--- packages/renderer/src/components/user-profile/partials/AvatarWebcam.tsx
+++ packages/renderer/src/components/user-profile/partials/AvatarWebcam.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 // Avatar Web cam using Native navigator media interface
 const AvatarWebcam = () => {
-  return <div></div>;
+    const camHeight = 150;
+    const camWidth = 150;
+    
+  return (
+    <div className='webcam'>
+        <div className="webcam__container">
+            <video 
+                height={camHeight}
+                width={camWidth}
+                muted 
+                autoPlay
+                className='webcam__video'
+                ></video>
+        </div>
+    </div>
+  );
 };
 
 export default AvatarWebcam;
